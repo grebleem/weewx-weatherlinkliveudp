599cae1a50e6749713c8d7681072c3af7ad3ecba
Bastiaan Meelberg <bastiaan@meelberg.com>
2020-06-24 11:41:51 +0200

    no message

--- install.py
+++ install.py
@@ -5,20 +5,20 @@
 from setup import ExtensionInstaller
 
 def loader():
-    return WLLInstaller()
+    return weatherlinkliveudp()
 
-class WLLInstaller(ExtensionInstaller):
+class weatherlinkliveudp(ExtensionInstaller):
     def __init__(self):
-        super(WLLInstaller, self).__init__(
-            version="0.1",
-            name='wll',
+        super(weatherlinkliveudp, self).__init__(
+            version="0.2",
+            name='weatherlinkliveudp',
             description='Periodically poll weather data from a WeatherLink Live device',
             author="Bastiaan Meelberg",
             config={
                 'WeatherLinkLiveUDP': {
                     'wll_ip': '1.2.3.4',
                     'poll_interval': 15,
-                    'ISS_id = 1'
+                    'ISS_id': 1,
                     'driver': 'user.weatherlinkliveudp'
                 }
             },
