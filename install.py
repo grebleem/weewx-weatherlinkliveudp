b37b88bb4f281c7fd1a8fec1733a2802e0fede5e
grebleem <bastiaan@meelberg.com>
2020-06-24 10:49:28 +0200

    Create install.py

---
+++ install.py
@@ -0,0 +1,24 @@
+# Installer file for WeatherLink Live (WeatherLinkLiveUDP) driver for WeeWX
+# Copyright 2020 Bastiaan Meelberg
+# Distributed under the terms of the GNU Public License (GPLv3)
+
+from setup import ExtensionInstaller
+
+def loader():
+    return WLLInstaller()
+
+class WLLInstaller(ExtensionInstaller):
+    def __init__(self):
+        super(WLLInstaller, self).__init__(
+            version="0.1",
+            name='wll',
+            description='Periodically poll weather data from a WeatherLink Live device',
+            author="Bastiaan Meelberg",
+            config={
+                'WeatherLinkLiveUDP': {
+                    'host': '1.2.3.4',
+                    'poll_interval': 15,
+                    'driver': 'user.weatherlinkliveudp'
+                }
+            },
+            files=[('bin/user', ['bin/user/weatherlinkliveudp.py'])])
