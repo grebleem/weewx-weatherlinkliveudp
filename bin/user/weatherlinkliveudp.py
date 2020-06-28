dafe94a028775520d4507f2e16f2f80a1ce75a8c
Bastiaan Meelberg <bastiaan@meelberg.com>
2020-06-28 10:09:32 +0200

    Cleanup of Code.

--- bin/user/weatherlinkliveudp.py
+++ bin/user/weatherlinkliveudp.py
@@ -16,7 +16,7 @@
 #
 
 # todo: Implementation of multiple transmitters.
-#
+
 """
 
 Weewx Driver for The WeatherLink Live (WLL). It implements a HTTP interface for getting current weather data and can support continuous requests as often as every 10 seconds. Also it collects a real-time 2.5 sec broadcast for wind speed and rain over UDP port 22222.
